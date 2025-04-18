af6e0ee8f24928482f31b26b56798eaa06f87fe6
soydantr <soydan@gmail.com>
2025-04-19 00:32:06 +0200

    Delete components directory

--- components/Navbar.tsx
+++
@@ -1,8 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="w-full flex items-center justify-between px-6 py-4 bg-white shadow">
-      <div className="text-xl font-bold text-blue-700">RENKMEN</div>
-      <div className="text-sm text-gray-600">🌐 TR | EN | ME</div>
-    </nav>
-  );
-}
